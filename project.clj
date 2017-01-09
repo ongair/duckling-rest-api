3d51e08052a87b64543a50a1e465727b818c3bfe
Trevor Kimenye <kimenye@gmail.com>
2017-01-09 15:57:17 +0300

    Initial commit

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject duckling-rest-api "0.1.0-SNAPSHOT"
+  :description "A simple restful api for accessing the duckling clojure library"
+  :url "http://ongair.im"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [ring/ring-json "0.4.0"]
+                 [ring/ring-jetty-adapter "1.5.0"]
+                 [ring/ring-defaults "0.2.1"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler duckling-rest-api.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
